6715d0b5084f1ec7fccf4059ddd9594dda55b4ed
Jexi Jiang <jexijiang@qq.com>
2024-10-24 21:10:30 +0800

    tag: find the err

--- algebra/executor.py
+++ algebra/executor.py
@@ -49,31 +49,31 @@ class DatabaseManager:
         except Exception as e:
             print(f"An error occurred while creating the database: {e}")
 
-    def use_database(self, database_name):
+    def select_database(self, database_name):
         self.database_name = database_name
-        self.database_path = os.path.join(self.db.root_dir, database_name)
-        self.log_path = os.path.join(self.db.database_path, 'log.txt')
+        self.database_path = os.path.join(self.root_dir, database_name)
+        self.log_path = os.path.join(self.database_path, 'log.txt')
 
-        print(f"Database Name: {self.db.database_name}")
-        print(f"Database Path: {self.db.database_path}")
-        print(f"Log Path: {self.db.log_path}")
+        print(f"Database Name: {self.database_name}")
+        print(f"Database Path: {self.database_path}")
+        print(f"Log Path: {self.log_path}")
 
 
-def create_database(database_name):
-    try:
-        # 如果根目录下database文件夹不存在，创建一个
-        root_dir = 'database'
-        if not os.path.exists(root_dir):
-            os.makedirs(root_dir)
-            print(f"Created root directory: {root_dir}")
-
-        database_path = os.path.join(root_dir, database_name)
-        if not os.path.exists(database_path):
-            os.makedirs(database_path)
-            print(f"Database '{database_name}' created at: {database_path}")
-            # 初始化日志
-            log.init_log(database_path)
-        else:
-            print(f"Database '{database_name}' already exists")
-    except Exception as e:
-        print(f"An error occurred while creating the database: {e}")
+# def create_database(database_name):
+#     try:
+#         # 如果根目录下database文件夹不存在，创建一个
+#         root_dir = 'database'
+#         if not os.path.exists(root_dir):
+#             os.makedirs(root_dir)
+#             print(f"Created root directory: {root_dir}")
+#
+#         database_path = os.path.join(root_dir, database_name)
+#         if not os.path.exists(database_path):
+#             os.makedirs(database_path)
+#             print(f"Database '{database_name}' created at: {database_path}")
+#             # 初始化日志
+#             log.init_log(database_path)
+#         else:
+#             print(f"Database '{database_name}' already exists")
+#     except Exception as e:
+#         print(f"An error occurred while creating the database: {e}")
