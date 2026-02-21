319e775f08de898af2aee30ac0a60e66bcb7c491
Jasper Jiang <williammilefer@gmail.com>
2026-02-22 00:24:59 +0800

    解决因为换行导致order_dateFROM相关token粘连的问题

--- utils/util.py
+++ utils/util.py
@@ -7,13 +7,19 @@ def user_input():
     while True:
         if info_input == '':
             # 第一次提示
-            info_input = input('SQL> ')
+            current_line = input('pysql> ')
+            info_input = current_line
         else:
             # 当输入不完整时，继续提示输入
-            info_input += input('>>>> ')
+            current_line = input('>>>> ')
+            # 🌟 关键修改：拼接时增加一个空格，防止跨行单词粘连
+            info_input += " " + current_line
+
         # 如果输入是 'exit;'，返回 None 表示退出
         if info_input.lower().strip() == 'exit;':
             return None
+        
+        # 检查是否以分号结尾（考虑到用户可能在分号后误打空格，用 strip）
         if ';' in info_input:
             return info_input.strip()
 
@@ -23,9 +29,8 @@ def timeit(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f" ({end_time - start_time:.4f} sec)")
+        print(f" ({end_time - start_time:.2f} sec)")
         return result
-
     return wrapper
 
 
