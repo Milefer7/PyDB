3357c09a26fd99d574bd41a729f88c843a4f497c
Jasper Jiang <williammilefer@gmail.com>
2026-02-24 21:52:41 +0800

    fix: 统计秒数格式调整

--- utils/util.py
+++ utils/util.py
@@ -29,7 +29,8 @@ def timeit(func):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f" ({end_time - start_time:.2f} sec)")
+        print(f" ({end_time - start_time:.2f} sec)", end="")
+        print()
         return result
     return wrapper
 
